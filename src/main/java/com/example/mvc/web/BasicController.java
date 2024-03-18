bc7fa09ecd0ff735a7a9d67f1056c77ea7b2ff7d
YoungjinLee <117518123+20zin@users.noreply.github.com>
2024-03-19 01:33:02 +0900

    4 commit

--- src/main/java/com/example/mvc/web/BasicController.java
+++ src/main/java/com/example/mvc/web/BasicController.java
@@ -6,10 +6,7 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -39,9 +36,52 @@ public class BasicController {
         return "basic/addForm";
     }
 
-    @PostMapping
-    public String save(){
+    //@PostMapping("/add")
+    public String save(@RequestParam String itemName,
+                       @RequestParam int price,
+                       @RequestParam Integer quantity,
+                       Model model){
+
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item", item);
         return "basic/addForm";
+    }
+
+    //@PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item){
+        itemRepository.save(item);
+//        model.addAttribute("item", item); //자동 추가, 생략 가능, 이름바꾸면 안됨
+        return "basic/item";
+    }
+
+    //modelattribute -> 1. 요청 파라미터 처리, 2.model 추가
+
+    //@PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item){
+        itemRepository.save(item);
+//        model.addAttribute("item", item); //자동 추가, 생략 가능, 이름바꾸면 안됨 , 이름지우면 클래스 이름의 앞글자 소문자로 해서 인식함
+        return "basic/item";
+    }
+
+    @PostMapping("/add")
+    public String addItemV4(Item item){
+        itemRepository.save(item);
+//        model.addAttribute("item", item); //자동 추가, 생략 가능, 이름바꾸면 안됨 , 이름지우면 클래스 이름의 앞글자 소문자로 해서 인식함
+        return "basic/item";
+    }
+    //자동 적용 됨
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model){
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
     }
 
 
